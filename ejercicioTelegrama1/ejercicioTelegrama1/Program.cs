52e6191e924035564409693a9c896c1038a694c9
Mehdi <choaffaba@alu.edu.gva.es>
2025-12-13 18:48:52 +0100

    codigo telegrama sin corregir

--- ejercicioTelegrama1/ejercicioTelegrama1/Program.cs
+++ ejercicioTelegrama1/ejercicioTelegrama1/Program.cs
@@ -10,6 +10,39 @@ namespace ejercicioTelegrama1
     {
         static void Main(string[] args)
         {
+            string texto;
+            int numPalabras;
+            double coste = 0;
+
+            Console.WriteLine("Introduce el telegrama:");
+            texto = Console.ReadLine();
+
+          
+            numPalabras = texto.Length;
+
+            Console.WriteLine("¿Es urgente? (s/n)");
+            char urgente = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            
+            if (urgente == 's')
+            {
+                if (numPalabras <= 10)
+                    coste = 5;
+                else
+                    coste = 0.75 * numPalabras;
+            }
+            else
+            {
+                if (numPalabras <= 10)
+                    coste = 2.5;
+                else
+                    coste = 0.5 * numPalabras;
+            }
+
+            Console.WriteLine("Coste: " + coste + " euros");
+            Console.ReadKey();
         }
+
     }
 }
